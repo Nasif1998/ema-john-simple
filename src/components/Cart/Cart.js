5d59703b945513098a51bfa4551364248c8d9c45
Nasif123 <nasif.reza@northsouth.edu>
2021-03-13 17:36:05 +0600

    first commit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,28 +1,29 @@
 import React from 'react';
 
+
 const Cart = (props) => {
     const cart = props.cart;
     // const totalPrice = cart.reduce((total, prd) => total + prd.price,0);
 
-    let total =0;
-    for(let i=0; i<cart.length; i++){
+    let total = 0;
+    for (let i = 0; i < cart.length; i++) {
         const product = cart[i];
-        total = total + product.price;
+        total = total + product.price * product.quantity;
     }
 
     let shipping = 0;
-    if(total >35){
+    if (total > 35) {
         shipping = 0;
     }
-    else if(total > 15){
-        shipping =4.99;
+    else if (total > 15) {
+        shipping = 4.99;
     }
-    else if(total > 0){
-        shipping =12.99;
+    else if (total > 0) {
+        shipping = 12.99;
     }
 
-    const tax = (total /10).toFixed(2);
-    const grandTotal = (total+shipping+Number(tax)).toFixed(2);
+    const tax = (total / 10).toFixed(2);
+    const grandTotal = (total + shipping + Number(tax)).toFixed(2);
     return (
         <div>
             <h4>Order Summery</h4>
@@ -31,6 +32,10 @@ const Cart = (props) => {
             <p><small>Shipping cost: {shipping}</small></p>
             <p><small>Tax+VAT: {tax}</small></p>
             <p>Total Price: {grandTotal}</p>
+            <br />
+            {
+                props.children
+            }
         </div>
     );
 };
---
+++ src/components/ReviewItem/ReviewItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const ReviewItem = (props) => {
+    const {name, quantity, key, price} = props.product;
+    const reviewItemStyle ={
+        borderBottom: '1px solid lightGray',
+        marginBottom: '5px',
+        paddingBottom: '5px',
+        marginLeft: '200px'
+    }
+    return (
+        <div style={reviewItemStyle}>
+            <h4>{name}</h4>
+            <p>Quantity: {quantity}</p>
+            <p><small>$ {price}</small></p>
+            <button
+             className="main-button"
+             onClick={() => props.removeProduct(key)}
+             >Remove</button>
+        </div>
+    );
+};
+
+export default ReviewItem;
